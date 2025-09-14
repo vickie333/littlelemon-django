dd78be73a7f9156b415d5f1dedf78e2c1b392ff9
vickie333 <mariavictoriaperezcontreras@gmail.com>
2025-09-14 11:47:11 -0400

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -25,7 +25,7 @@ env.read_env(os.path.join(ENV_DIR, '.env'))
 
 SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('Debug', default=False)
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
